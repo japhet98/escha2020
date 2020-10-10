eb46e1917ce04373a03d618cd13a390e33551790
japhet98 <37064005+japhet98@users.noreply.github.com>
2020-10-10 14:10:15 +0000

    build

---
+++ build/precache-manifest.4d09874902b7f826c6b5dc669ce27f84.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a790595acae333f0c659affae75eeb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f815f7954f662547527",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "01144b41c5ccec123791",
+    "url": "/static/css/main.f50af112.chunk.css"
+  },
+  {
+    "revision": "7f815f7954f662547527",
+    "url": "/static/js/2.3315c444.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.3315c444.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01144b41c5ccec123791",
+    "url": "/static/js/main.67fa8d5d.chunk.js"
+  },
+  {
+    "revision": "3c1a0e17234533ca4547",
+    "url": "/static/js/runtime-main.7e9b086a.js"
+  },
+  {
+    "revision": "6c2f8c4793d8593f72d08c83a927707c",
+    "url": "/static/media/IMG-20201007-WA0008.6c2f8c47.jpg"
+  }
+]);
